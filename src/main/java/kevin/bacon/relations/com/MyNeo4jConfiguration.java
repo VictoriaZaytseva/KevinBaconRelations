02849c1a90f1e66f015c8007ebd7b83d9a29894d
VictoriaZaytseva <vika01011001@gmail.com>
2016-07-29 10:24:59 -0400

    Update MyNeo4jConfiguration.java

--- src/main/java/kevin/bacon/relations/com/MyNeo4jConfiguration.java
+++ src/main/java/kevin/bacon/relations/com/MyNeo4jConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+// NEOj4 Configuration is borrowed from example application
 @EnableTransactionManagement
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableScheduling
