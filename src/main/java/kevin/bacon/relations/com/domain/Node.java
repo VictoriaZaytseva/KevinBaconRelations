2bab91aa0034e5357b2dab60a69773627548f9a2
VictoriaZaytseva <vika01011001@gmail.com>
2016-07-29 10:30:17 -0400

    Update Node.java

--- src/main/java/kevin/bacon/relations/com/domain/Node.java
+++ src/main/java/kevin/bacon/relations/com/domain/Node.java
@@ -9,22 +9,26 @@ import org.neo4j.ogm.annotation.GraphId;
  *
  */
 public class Node {
-	@GraphId Long id;
+    
+    @GraphId Long id;
     protected String name;
     protected String image;
-    
+   
     public Node(){}
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setImage(String image) {
         this.image = image;
     }
+    
     public String getImage() {
         return image;
     }
-    
 }
