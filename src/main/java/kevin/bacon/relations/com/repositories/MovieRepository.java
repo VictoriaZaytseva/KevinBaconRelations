d7bb66df4827f25ca84332b8495d909a6fd9954a
VictoriaZaytseva <vika01011001@gmail.com>
2016-07-29 10:18:39 -0400

    Update MovieRepository.java

--- src/main/java/kevin/bacon/relations/com/repositories/MovieRepository.java
+++ src/main/java/kevin/bacon/relations/com/repositories/MovieRepository.java
@@ -11,20 +11,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author mh
- * @since 24.07.12
- */
-// tag::repository[]
 @RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
 public interface MovieRepository extends GraphRepository<Movie> {
-    Movie findByName(@Param("name") String name);
-
-    @Query("MATCH p=shortestPath((bacon:Actor {name:\"Kevin Bacon\"})-[*]-(meg:Actor {name:{name}}))RETURN NODES(p)")
-    Iterable<Map<String, Object>> findShortestPathTo(@Param("name") String name);
-
     @Query("MATCH (m:Movie)<-[:ACTED_IN]-(a:Actor) RETURN m.name as movie, collect(a.name) as cast LIMIT {limit}")
     List<Map<String,Object>> graph(@Param("limit") int limit);
 }
-// end::repository[]
 
